43958f8a2d4b36cb2ed8f7f86c029a1fc7732b0a
aswinbennyofficial <aswinbenny.official@gmail.com>
2024-10-13 20:15:00 +0530

    Added logic for db connection

---
+++ backend/db.php
@@ -0,0 +1,20 @@
+<?php
+function getDbConnection() {
+    $host = getenv('DB_HOST');
+    $dbname = getenv('DB_NAME');
+    $user = getenv('DB_USER');
+    $pass = getenv('DB_PASS');
+
+    $dsn = "pgsql:host=$host;dbname=$dbname";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+
+    try {
+        return new PDO($dsn, $user, $pass, $options);
+    } catch (PDOException $e) {
+        throw new PDOException($e->getMessage(), (int)$e->getCode());
+    }
+}
